e1005fd0b5e66f60757daa9fb64d97a38cc22acf
Kelli Horacek <kelliscoding@gmail.com>
2023-04-05 12:12:15 -0500

    Complete Phase 1 Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+
+const multiply = num1 * num2;
+
+let random = Number.parseInt((Math.random() * 10) + 1);
+
+let num3 = 20;
+let num4 = 8;
+
+const mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 20, 10);
